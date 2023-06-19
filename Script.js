b6314887905649c47759828f013278a5f8c13cb1
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2023-06-19 19:36:39 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -2,6 +2,7 @@
     let speed = 100;
 
     /*  SCENE 1 */
+
     let scene1 = gsap.timeline();
     ScrollTrigger.create({
         animation: scene1,
@@ -12,6 +13,7 @@
     });
 
     // hills animation 
+
     scene1.to("#h1-1", { y: 3 * speed, x: 1 * speed, scale: 0.9, ease: "power1.in" }, 0)
     scene1.to("#h1-2", { y: 2.6 * speed, x: -0.6 * speed, ease: "power1.in" }, 0)
     scene1.to("#h1-3", { y: 1.7 * speed, x: 1.2 * speed }, 0.03)
